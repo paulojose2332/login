566572cd84ef21c5eca89e8b6ad7ee56886b03f3
paulojose2332 <paulo.jose.n@outlook.com>
2019-03-22 10:33:16 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Screen from "./screens/index";
 
-//class App extends React.Component {
-//    render() {
-//        return (
-//            <Login />
-//        );
-//    }
-//}
 
-// <Route path="/users" component={Users} />
 ReactDOM.render(<Screen />, document.querySelector("#root"));
